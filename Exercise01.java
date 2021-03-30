20c42a1bc7b86db1ec42702ecc508694ace8d6e8
Pjiv657 <54612423+Pjiv657@users.noreply.github.com>
2021-03-30 13:51:32 +1300

    Finished Palindrome function

--- Exercise01.java
+++ Exercise01.java
@@ -7,9 +7,9 @@ public class ExerciseOne {
       System.out.println(ex01.reverseString("Bsuiness School"));
       
       // Uncomment the following statements after finishing Exercise Four
-    //   System.out.println(ex01.isStringPalindrome("hello world"));
-    //   System.out.println(ex01.isStringPalindrome("Dad"));
-    //   System.out.println(ex01.isStringPalindrome("radar"));
+       System.out.println(ex01.isStringPalindrome("hello world"));
+       System.out.println(ex01.isStringPalindrome("Dad"));
+       System.out.println(ex01.isStringPalindrome("radar"));
     }
     
     // Exercise One
@@ -19,7 +19,8 @@ public class ExerciseOne {
         //for each letter, append to new word, return new word
         String revWord = "";
         for (int i = 0; i < word.length();i++){
-            revWord = revWord + word.charAt(word.length() - i - 1);   
+            //revWord = revWord + word.charAt(word.length() - i - 1);
+            revWord = word.charAt(i) + revWord;
         }
         return revWord;
     }
@@ -27,6 +28,11 @@ public class ExerciseOne {
     // Exercise Four
     private boolean isStringPalindrome(String word) {
         // Complete this method to deermine if the given word is a palindrome
-        return false;
+        for (int i=0;i<word.length()/2;i++){
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
