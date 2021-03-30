6de63a2f58623bc0c4213ee23e3a5a7eca573c51
Pjiv657 <54612423+Pjiv657@users.noreply.github.com>
2021-03-30 13:32:41 +1300

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -1,4 +1,16 @@
 # Exercise Two
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
+def fiboseq(index):
+  prevnum = 0
+  temp = 0
+  currnum = 1
+  for x in range(1, index):
+    temp = currnum
+    currnum = prevnum + temp
+    prevnum = temp
+    
+  return currnum
 
+index = 6
+print("The value at the "+str(index)+"th position in the Fibonacci sequence is "+str(fiboseq(index)))
 print("The value at the 6th postion in the Fibonacci sequence is 8")
