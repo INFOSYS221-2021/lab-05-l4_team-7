5eac4eda33cebe746d4f0669cb290c72a3ce6476
Pjiv657 <54612423+Pjiv657@users.noreply.github.com>
2021-03-30 13:39:55 +1300

    made countdig function

--- exercise03.py
+++ exercise03.py
@@ -1,4 +1,12 @@
 # Exercise Three
 # Write a simple program that finds the number of digits of a given integer value
-
-print("The number of digits for a value of 100 is 3")
+def countdig(num):
+  count = 0
+  if num < 0:
+    count = -1
+  for x in str(num):
+    count += 1
+  return count
+num = 100
+print("The number of digits for a value of "+str(num) + " is " +str(countdig(num)))
+#print("The number of digits for a value of 100 is 3")
